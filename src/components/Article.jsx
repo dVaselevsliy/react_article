6c3c5029460d69b6ba9e547ac5b2e46450e697c1
Vaselevskiy Denys <denys020107@gmail.com>
2024-02-25 21:01:52 +0200

    first commit

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,15 @@
-// export const Article = () => ();
+export const Article = ({ title, date, text }) => (
+  <div className="Article">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime="2019-08-30" data-cy="date">
+        {date}
+      </time>
+    </p>
+
+    <p data-cy="text">{text}</p>
+  </div>
+);
